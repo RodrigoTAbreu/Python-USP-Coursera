09e587c9b1f1c227f68dd057ffc9fc63745b5aed
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2022-03-27 07:20:12 -0300

    n_Primos

--- qtdPrimos.py
+++ qtdPrimos.py
@@ -1,19 +1,19 @@
-n = int(input("Informe o numero: "))
-
-cont = 0
-
-for count in range(2,n):
-    count += 1
-    if count == 2 :
+def n_primos(n):
+    count = 0
+    cont = 0
+    if n == 2:
         cont = 1
-        #print(f"{n} é primo.")
-    else:
+
+    for count in range(2,n):
+        count += 1
         if n % 2 == 0:
             #print(f"{n} não é primo.")
             cont +=1
         else:
             #print(f"{n} é primo.")
             cont += 1
-#print(count)
-print(cont)
 
+    print(cont)
+
+n = int(input("Informe o numero: "))
+n_primos(n)
