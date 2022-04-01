ffeaed8b630421b4f3c78508c7eb3f0bdcc681a6
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2022-04-01 06:15:02 -0300

    FOR
    Exercícios de for

--- Listas e FOR.py
+++ Listas e FOR.py
@@ -20,8 +20,74 @@ numeros = [50,10,15,35,30,20,5,1,2]
 # for x in numeros:
 #      print(x)
 
-print(f"Lista de numeros {numeros}")
-for i in range(len(numeros)):
-    numeros[i] = numeros[i]**3
+# print(f"Lista de numeros {numeros}")
+# for i in range(len(numeros)):
+#     numeros[i] = numeros[i]**3
+#
+# print(f"Lista de numeros ao cubo {numeros}")
 
-print(f"Lista de numeros ao cubo {numeros}")
+# animais = ["gato", "cachorro", "papagaio", "arara", "jacaré"]
+#
+# for x in range(len(animais)):
+#     print("--> ", x)
+
+
+# #pares = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+# pares = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+# for i in range(0, 50, 5):
+# 	print(i)
+
+valores = []
+for i in range(1, 10):
+    if i % 2 == 0:
+        valores.append(i)
+
+print(valores)
+
+print("*"*20)
+
+# valores = []
+# for i in range(0, 10, 2):
+#     valores.append(i)
+#
+# print(valores)
+
+# valores = []
+# for i in range(2, 10, 2):
+#     valores.append(i)
+#
+# print(valores)
+
+# valores = []
+# for i in range(1, 10, 2):
+#     valores.append(i)
+#
+# print(valores)
+
+#
+# animais = ["gato", "cachorro", "papagaio",
+# "arara", "jacaré"]
+#
+# #animais[2] = "piriquito"
+# #animais[2] = animais.append("piriquito")
+# animais[-3] = "LEAO"
+
+# print(animais)
+
+# for i in range(0, 50, 5):
+#     print(i)
+
+# pares = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+# for x in range(5, 10):
+#     print(pares[x])
+
+# pares = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+# for x in range(len(pares)):
+#     print(x)
+
+# pares = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+# for x in range(len(pares)):
+#     print(pares[x])
+
+for i in range(16,4,-3):
+	print(i)
