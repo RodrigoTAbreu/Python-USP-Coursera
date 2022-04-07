725d51e75d63ad62e6247e1f76166fdc46ff96d1
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2022-04-07 06:41:04 -0300

    Testes Finais

--- Teste de Codigo.py
+++ Teste de Codigo.py
@@ -1,5 +1,20 @@
-def func(x):
-    return x + 1
+# def func(x):
+#     return x + 1
+#
+# def test_answer():
+# #     assert func(3) == 4
+#
+# res = 42 % 10
+# x = 1 + 2 * 3 - 8 / 4
+# print(res)
+# print(x)
+# y= int(98.6)
+# print(y)
+#
+# name = input("Enter your name")
+# print("Howdy"+ name)
 
-def test_answer():
-    assert func(3) == 4
+hrs = int(input("Entre com as Horas: "))
+vlr_hr = 2.75
+resp = hrs * vlr_hr
+print(resp)
