de5770df4004eef0feda726982c808ebde8d6687
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2022-06-18 07:00:34 -0300

    up

--- Mod-02/Exer-02-02.py
+++ Mod-02/Exer-02-02.py
@@ -3,4 +3,9 @@ m1 = [[1, 2, 3], [4, 5, 6]]
 m2 = [[2, 3, 4], [5, 6, 7]]
 
 if len(m1) == len(m2):
-    print(m1[1][2])
+    tam1 = len(m1)
+    tam2 = len(m2)
+    for i in range(tam1):
+        for j in range(tam2):
+            elemento1 = m1[i][j] + m2[i][j]
+            print(elemento1)
