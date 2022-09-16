3cea22f59397b9ff1cc1df96a80bee46ad4dbe07
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2022-09-16 06:27:46 -0300

    add exercicios
    add

--- Mod-02/BubleSort.py
+++ Mod-02/BubleSort.py
@@ -8,4 +8,7 @@ def bubble_sort(list: list) -> list:
                 list[j], list[j+1] = list[j+1], list[j]
                 print(list)
 
-    return list
+    print(list)
+
+list = [-1,3,8,6,17,4,10,-5,50,45,15,9]
+nome = bubble_sort(list)
